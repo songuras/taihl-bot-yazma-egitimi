1537f794561aed1748e7c38603d15484d353d14c
Ahmet Serdar SONGUR <songuras51@gmail.com>
2022-06-25 20:48:21 +0300

    ilk soru yapıldı

---
+++ uygulama_1_cozum.py
@@ -0,0 +1,8 @@
+#soru 1
+"""
+ad = input("öğrencinin adı : ")
+birinci = int(input("birinci sınav notu : "))
+ikinci = int(input("ikinci sınav notu : "))
+ort = (birinci+ikinci)/2
+print(f"öğrencinin adı : {ad} sınav ortalaması : {ort}")
+"""
